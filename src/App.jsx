420f187817ca7798750b984534cb6ecce93d2f33
Fernando <ferxx18@gmail.com>
2024-01-29 23:27:57 -0600

    primeros archivos

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css"
+import { Navbar } from "./Navbar"
+import { Footer } from "./Footer"
+
+function App() {
+
+
+  return (
+    <div>
+      <Navbar />
+      <h1 className="tittle">Hola Mundo</h1>
+      <Footer />
+    </div>    
+  )
+}
+
+export default App
---
+++ src/Navbar.jsx
@@ -0,0 +1,12 @@
+import "./Navbar.css"
+
+export const Navbar = ()=>{
+    return <nav>
+        <ul>
+            <li className="navTiitle">Inicio</li>
+            <li>Blog</li>
+            <li>Gallery</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+}
