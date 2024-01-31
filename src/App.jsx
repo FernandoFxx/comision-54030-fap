ea118e08d398b738bf9d7d452f7e9e6f568c7a77
Fernando <ferxx18@gmail.com>
2024-01-31 15:03:52 -0600

    project clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import "./App.css"
-import { Navbar } from "./Navbar"
-import { Footer } from "./Footer"
 
 function App() {
 
 
   return (
     <div>
-      <Navbar />
       <h1 className="tittle">Hola Mundo</h1>
-      <Footer />
     </div>    
   )
 }
